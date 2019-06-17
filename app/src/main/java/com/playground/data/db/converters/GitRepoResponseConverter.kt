33ef4a6b5320c0ec916207433df10bd5af3fa2a0
Enrico Bruno Del Zotto <delzotto.enrico@gmail.com>
2019-06-17 17:43:37 +0200

    Issue on deserialize with BaseObjectConverter, TBF

--- app/src/main/java/com/playground/data/db/converters/GitRepoResponseConverter.kt
+++ app/src/main/java/com/playground/data/db/converters/GitRepoResponseConverter.kt
@@ -1,5 +1,27 @@
 package com.playground.data.db.converters
 
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import com.playground.models.GitRepo
+import java.util.*
 
-class GitRepoResponseConverter : BaseObjectConverter<GitRepo>()
+class GitRepoResponseConverter
+{
+
+    private var gson = Gson()
+
+    @TypeConverter
+    fun fromGson(data: String?): List<GitRepo> {
+        if (data == null) {
+            return Collections.emptyList()
+        }
+        val listType = object : TypeToken<List<GitRepo>>() {}.type
+        return gson.fromJson(data, listType)
+    }
+
+    @TypeConverter
+    fun toGson(someObjects: List<GitRepo>): String {
+        return gson.toJson(someObjects)
+    }
+}
