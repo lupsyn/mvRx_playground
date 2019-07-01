ee7efd7763eabf87679f43800388611a84a428c5
Enrico Bruno Del Zotto <delzotto.enrico@gmail.com>
2019-07-01 22:57:50 +0200

    Fixing clear env every time Espresso test is starting.

--- app/src/androidTest/java/com/playground/utils/rules/ClearAppDataRule.kt
+++ app/src/androidTest/java/com/playground/utils/rules/ClearAppDataRule.kt
@@ -3,6 +3,7 @@ package com.playground.utils.rules
 import androidx.test.core.app.ApplicationProvider
 import com.playground.base.MvRxTestApplication
 import com.playground.data.db.MvRxDb
+import com.playground.utils.SystemUtils
 import org.junit.rules.ExternalResource
 import org.koin.android.ext.android.inject
 
@@ -16,6 +17,9 @@ class ClearAppDataRule : ExternalResource() {
     private val db: MvRxDb  by ApplicationProvider.getApplicationContext<MvRxTestApplication>().inject()
 
     override fun before() {
+        if (SystemUtils.isART()) {
+            db.clearAllTables()
+        }
         super.before()
     }
 }
